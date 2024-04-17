06616e38f28a86780e4ed76f9dcefcfef62dc231
Omar Adobati <omar.adobati@publicissapient.com>
2024-04-17 10:39:40 +0200

    initial commit

---
+++ packages/react-library/lib/components/stencil-generated/index.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from 'stencil-library/dist/components';
+
+import { defineCustomElement as defineMyComponent1 } from 'stencil-library/dist/components/my-component-1.js';
+import { defineCustomElement as defineMyComponent2 } from 'stencil-library/dist/components/my-component-2.js';
+
+export const MyComponent1 = /*@__PURE__*/createReactComponent<JSX.MyComponent1, HTMLMyComponent1Element>('my-component-1', undefined, undefined, defineMyComponent1);
+export const MyComponent2 = /*@__PURE__*/createReactComponent<JSX.MyComponent2, HTMLMyComponent2Element>('my-component-2', undefined, undefined, defineMyComponent2);
